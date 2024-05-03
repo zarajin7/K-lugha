0ac5d7abc7e91105ecec1a66e36ebf633e438488
emmanuellumwachi <lumwachiemmanuel18@gmail.com>
2024-05-03 10:17:26 +0300

    added files

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCRyqNQj0nHElpi0e3po4gt6GUAhV7hl6I",
-  authDomain: "k-lugha.firebaseapp.com",
-  projectId: "k-lugha",
-  storageBucket: "k-lugha.appspot.com",
-  messagingSenderId: "1066933439994",
-  appId: "1:1066933439994:web:a33aa474b1ed1ec171a843",
-  measurementId: "G-0JRHDMFTCC",
+  apiKey: "AIzaSyDdby9adwpz8EspNhd0JIzEdAhf00CAMQg",
+  authDomain: "k-lugha-be2c6.firebaseapp.com",
+  projectId: "k-lugha-be2c6",
+  storageBucket: "k-lugha-be2c6.appspot.com",
+  messagingSenderId: "48756828498",
+  appId: "1:48756828498:web:471195a4ea941238aa328b",
+  measurementId: "G-C263MSVGM0"
 };
 
 // Initialize Firebase
